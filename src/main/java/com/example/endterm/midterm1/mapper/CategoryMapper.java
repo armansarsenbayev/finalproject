da4f4d0de437a546c80562b21dc6913f91a9f1ca
Arman Sarsenbayev <armansarsenbayev@narxoz.kz>
2025-12-20 01:15:13 +0500

    .

---
+++ src/main/java/com/example/endterm/midterm1/mapper/CategoryMapper.java
@@ -0,0 +1,21 @@
+package com.example.endterm.midterm1.mapper;
+
+import com.example.endterm.midterm1.dto.CategoryDto;
+import com.example.endterm.midterm1.dto.CountryDto;
+import com.example.endterm.midterm1.entity.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+
+    @Mapping(target = "caregoryDto", source = "category")
+    CategoryDto toDto(Category category);
+
+    @Mapping(target = "caregory", source = "categoryDto")
+    Category toEntity(CategoryDto dto);
+
+    List<CategoryDto> toDtoList(List<Category> categories);
+}
