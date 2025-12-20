8e386925cb8fc40532176e40894ed1d6cf4c934d
Arman Sarsenbayev <armansarsenbayev@narxoz.kz>
2025-12-20 17:29:00 +0500

    .

--- src/main/java/com/example/endterm/midterm1/dto/CountryDto.java
+++ src/main/java/com/example/endterm/midterm1/dto/CountryDto.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.Builder;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class CountryDto{
     private Long id;
     private String codeDto;
